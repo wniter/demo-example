4a77864223d0fdc83cac3e6b5810a6bb003a5dfa
Boolean <lyg2000@aliyun.com>
2021-12-28 00:27:34 +0800

    modify pom  and finish web

---
+++ src/test/java/com/example/demo/demoTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @create 2021-12-28 0:17
+ */
+public class demoTest {
+    @Test
+    public void test() throws Exception {
+
+    }
+}
