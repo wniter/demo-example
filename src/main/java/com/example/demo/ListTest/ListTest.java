b7dcec5d6ede7b72cc82d10ca6021618994d3f9c
Boolean <lyg2000@aliyun.com>
2021-12-29 21:11:37 +0800

    Create ListTest.java

---
+++ src/main/java/com/example/demo/ListTest/ListTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.ListTest;
+
+/**
+ * @create 2021-12-29 21:08
+ */
+public class ListTest {
+    public static void main(String[] args) {
+        //add,size,get,isEmpty,addAll,toArray,contains,remove,iterator,clear,stream,forEach,set,subList,indexOf,
+        //list.toArray()转换为数组对象
+        //contains()测试此列表是否包含指定的对象。
+    }
+}
