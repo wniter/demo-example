a3e2de6ddd9ee2fd72a5de28eda3f0f1f8bb8720
Boolean <lyg2000@aliyun.com>
2022-01-27 07:11:51 +0800

    Coroutine

---
+++ src/main/java/com/example/juc/coroutine/Coroutine.java
@@ -0,0 +1,30 @@
+package com.example.juc.coroutine;
+
+
+
+import kilim.Pausable;
+import kilim.Task;
+/**
+ * @create 2022-01-27 6:44
+ */
+//Coroutine-demo
+public class Coroutine {
+    public static void main(String[] args) {
+        Task task  = new Task() {
+            @Override
+            public  void execute() throws Pausable {
+                System.out.println("hello");
+                yield();
+                System.out.println("hello -1");
+                yield();
+                System.out.println("finish");
+            }
+        };
+        task.run();
+        System.out.println("world");
+        task.run();
+        System.out.println("world-1");
+        task.run();
+
+    }
+}
