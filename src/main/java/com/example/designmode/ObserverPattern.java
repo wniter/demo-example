e2e7cf27d29f8597df8e155a2a40a20b15d48e6a
Boolean <lyg2000@aliyun.com>
2022-02-13 21:30:08 +0800

    add

---
+++ src/main/java/com/example/designmode/ObserverPattern.java
@@ -0,0 +1,101 @@
+package com.example.designmode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 观察者模式
+ * 当一个对象被修改时，则会自动通知依赖它的对象。观察者模式属于行为型模式。
+ * @create 2022-02-13 21:13
+ */
+public class ObserverPattern {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        new HexaObserver(subject);
+        new OctalObserver(subject);
+        new BinaryObserver(subject);
+
+        System.out.println("First state change: 15");
+        subject.setState(15);
+        System.out.println("Second state change: 10");
+        subject.setState(10);
+    }
+}
+
+//创建 Subject 类。
+class Subject {
+
+    private List<Observer> observers
+            = new ArrayList<Observer>();
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
+
+//创建 Observer 类。
+abstract class Observer {
+    protected Subject subject;
+
+    public abstract void update();
+}
+
+//创建实体观察者类。
+class BinaryObserver extends Observer {
+
+    public BinaryObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Binary String: "
+                + Integer.toBinaryString(subject.getState()));
+    }
+}
+
+class OctalObserver extends Observer {
+
+    public OctalObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Octal String: "
+                + Integer.toOctalString(subject.getState()));
+    }
+}
+
+class HexaObserver extends Observer {
+
+    public HexaObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Hex String: "
+                + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+}
